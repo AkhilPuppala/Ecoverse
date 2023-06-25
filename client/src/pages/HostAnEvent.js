5f67b004c28220ea3806f167687fdeff6cc45c7b
Raghunadh2004 <98640541+Raghunadh2004@users.noreply.github.com>
2023-06-25 20:09:28 +0530

    Update HostAnEvent.js

--- client/src/pages/HostAnEvent.js
+++ client/src/pages/HostAnEvent.js
@@ -4,7 +4,24 @@ import Layout from '../components/Layout/Layout'
 const HostAnEvent = () => {
   return (
     <Layout>
-        <h1>HostAnEvent</h1>
+        <div className='container'>
+        <h1 className='text-center'>Host An Event</h1>
+        <form>
+          <div className="mb-3">
+            <input type="text" className="form-control" id="exampleInputTitle1"  placeholder='Enter title of the event'/>
+          </div>
+          <div className="mb-3">
+            <input type="text" className="form-control" id="exampleInputDescription1"  placeholder='Enter description of the event'/>
+          </div>
+          <div className="mb-3">
+            <input type="text" className="form-control" id="exampleInputPlace1"  placeholder='Enter place of the event'/>
+          </div>
+          <div className="mb-3">
+            <input type="text" className="form-control" id="exampleInputDate1"  placeholder='Enter date of the event'/>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+        </div>
     </Layout>
   )
 }
